42c59bd62094688898ca074baf99b4645fede6b4
gvalmana <gustavo.shotokai@gmail.com>
2023-11-30 16:14:40 -0500

    agregadas constantes y directorios

---
+++ gvalmana_cv_reflex/constants.py
@@ -0,0 +1,6 @@
+# Cabecera
+GITHUB_URL = "https://github.com/gvalmana"
+TWITTER_X_URL = "https://twitter.com/GustavoValmana"
+INSTAGRAM_URL = "https://www.instagram.com/gustavovalmanavillalonga/"
+LINKEDIN_URL = "https://www.linkedin.com/in/gustavo-valma%C3%B1a-villalonga-9a5b95119/"
+FACEBOOK_URL = "https://www.facebook.com/gustavo.valmanavillalonga/"
